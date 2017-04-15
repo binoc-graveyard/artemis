410fa4a1e1ec3978ad315376e022fb9ec0f86c4a
Matt A. Tobin <email@mattatobin.com>
2017-04-14 22:06:41 -0400

    no message

--- artemis/modules/funcReadManifest.php
+++ artemis/modules/funcReadManifest.php
@@ -6,9 +6,9 @@ function funcReadManifest($_slug) {
     $_manifestFile = 'artemus.manifest';
     
     if (file_exists($GLOBALS['strArtemisDatastore'] . $_slug . '/' . $_manifestFile)) {
-        $_manifest = file_get_contents($GLOBALS['strArtemisDatastore'] . $_slug . '/' . $_manifestFile)
+        $_manifestRaw = file_get_contents($GLOBALS['strArtemisDatastore'] . $_slug . '/' . $_manifestFile)
             or funcError('Could not find manifest file for ' . $_slug);
-        $_manifest = json_decode($_manfiest, true);
+        $_manifest = json_decode($_manifestRaw, true);
     }
     else {
         funcError('Could not find manifest file for ' . $_slug);
